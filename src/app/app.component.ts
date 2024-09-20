61e6602c14195f885a4c4f39de7c9e7ff52d4b1a
Vladimir Varbanov <v.varbanov@eos-matrix.bg>
2024-09-20 16:49:26 +0300

    feature: a try without zone js. experimental feature of the angular 18.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @Input() name: string = 'm3-management';
   title = 'm3-management';
 }
